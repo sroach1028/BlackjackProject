fc7e3204e749b05b3831dbba2c6635b0675c92a4
Shaun Roach <sroach1028@gmail.com>
2019-10-05 23:06:01 -0600

    broke a lot of things, now have them fixed

--- src/com/skilldistillery/blackjack/BlackJackTable.java
+++ src/com/skilldistillery/blackjack/BlackJackTable.java
@@ -11,8 +11,8 @@ public class BlackJackTable {
 
 	public void startGame() {
 		dealer.deck.shuffle();
-		System.out.println("Let's play blackjack! Cards are shuffled. Dealing out\n...\n...");
-		dealHands();
+		System.out.println("Let's play blackjack! Cards are shuffled.");
+		play();
 	}
 
 	public void dealHands() {
@@ -22,75 +22,108 @@ public class BlackJackTable {
 		p1.showCards();
 		dealer.addCard(dealer.deck.dealCard());
 		dealer.showOneCard();
-		play();
 	}
 
 	public void play() {
 		do {
-			playerPlays();
-			if (p1.hand.isBust()) {
-				System.out.println(p1.hand.getValue() + " player busts\nDealer Wins..");
-				System.out.println();
-			} else {
 
-				checkForWinner();
+			System.out.println("\n/Dealing cards...");
+			dealHands();
+
+			if (p1.hand.isBlackJack() && dealer.hand.isBlackJack()) {
+				dealer.showCards();
+				System.out.println("Player has blackjack...Dealer has blackjack. Push");
+				discardHands();
+				continue;
+			} else if (p1.hand.isBlackJack()) {
+				System.out.println("Player has Blackjack, player wins");
+				discardHands();
+				continue;
 			}
+
+			else if (dealer.hand.isBlackJack()) {
+				dealer.showCards();
+				System.out.println("Dealer has BlackJack...dealer wins...");
+				discardHands();
+				continue;
+			}
+
+			else {
+				playerPlays();
+			}
+
+			if (p1.hand.isBust()) {
+				System.out.println(p1.hand.getValue() + " player busts\nDealer Wins..\n");
+				p1.hand.discardHand();
+				dealer.hand.discardHand();
+				continue;
+			} else {
+				dealerPlays();
+				
+			}
+
+			if (dealer.hand.isBust()) {
+				System.out.println(dealer.hand.getValue() + " Dealer Busts\nPlayer Wins!");
+				p1.hand.discardHand();
+				dealer.hand.discardHand();
+				continue;
+			} else {
+				checkForWinner();
+				p1.hand.discardHand();
+				dealer.hand.discardHand();
+			}
+
 		} while (playAgain());
 
 	}
 
 	public void playerPlays() {
 		Scanner kb = new Scanner(System.in);
-		while (!p1.hand.isBust()) {
+		do {
 			System.out.println("\nPlayer has " + p1.hand.getValue());
 			System.out.println("1. Hit\n2. Stay");
-			switch (kb.nextInt()) {
-			case 1:
+			int choice = kb.nextInt();
+			if (choice == 1) {
 				p1.hit(dealer.deck.dealCard());
-				break;
-			case 2:
+			}
+			if (choice == 2) {
 				System.out.println("Player stays at " + p1.hand.getValue());
-				dealerPlays();
+				break;
 			}
-			if (p1.hand.is21()) {
-				System.out.println("Player has 21, dealer turn");
-				dealerPlays();
-			}
-		}
 
+			if (p1.hand.is21()) {
+				System.out.println("Player has 21, dealer turn\n");
+				break;
+			}
+		} while (!p1.hand.isBust());
 	}
 
 	public void dealerPlays() {
-		dealer.showCards();
-		while (!dealer.hand.isBust()) {
+		do {
+
+			dealer.showCards();
 
 			if (!dealer.hand.is17()) {
 				dealer.hit(dealer.deck.dealCard());
-				dealer.showCards();
+				continue;
 			}
 
 			else {
-				checkForWinner();
+				break;
 			}
 
-		}
-		if (dealer.hand.isBust()) {
-			System.out.println(dealer.hand.getValue() + " Dealer Busts\nPlayer Wins!");
-			playAgain();
-		}
+		} while (!dealer.hand.isBust());
+		dealer.showCards();
 	}
 
 	public void checkForWinner() {
-		System.out.println("Dealer has " + dealer.hand.getValue());
+		System.out.println("dealer has  " + dealer.hand.getValue());
 		if (p1.hand.getValue() > dealer.hand.getValue()) {
 			System.out.println("Player wins");
-			playAgain();
 		} else if (p1.hand.getValue() < dealer.hand.getValue()) {
 			System.out.println("Dealer wins");
-			playAgain();
 		} else if (p1.hand.getValue() == dealer.hand.getValue()) {
 			System.out.println("Push");
-			playAgain();
 		}
 	}
 
@@ -100,15 +133,18 @@ public class BlackJackTable {
 		switch (kb.next()) {
 		case "y":
 		case "Y":
-			p1.hand.discardHand();
-			dealer.hand.discardHand();
-			dealHands();
+			return true;
 		case "N":
 		case "n":
-			System.out.println("Goodbye");
-			System.exit(0);
+			System.out.println("Goodbye...");
+			break;
 		}
 		return false;
+	}
+	
+	public void discardHands() {
+		p1.hand.discardHand();
+		dealer.hand.discardHand();
 	}
 
 }
